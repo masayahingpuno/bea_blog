06278e35ec9b0008885f286545d3966b56fdd212
masayahingpuno <rudyquismorio@gmail.com>
2023-05-19 21:18:36 +0800

    Update +page.ts

--- src/routes/blog/[blogId]/+page.ts
+++ src/routes/blog/[blogId]/+page.ts
@@ -1,15 +1,14 @@
 import type { PageLoad } from './$types';
-import { posts } from '$lib/utility/posts';
+// import { posts } from '$lib/utility/posts';
 
 
 export const load = (async ({ params }) => {
     // load the markdown file based on slug
     const component = await import(`../../../lib/blogs/${params.blogId}.md`);
-    const post = posts.filter((post) => post.slug === params.blogId)[0];
+    // const post = posts.filter((post) => post.slug === params.blogId)[0];
 
     return {
         blog: params.blogId,
-        post,
         component: component.default,
         layout: {
         fullWidth: true,
