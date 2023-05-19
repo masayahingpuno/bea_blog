cd1300f9e2066fad42b36d032849103380793bfe
masayahingpuno <rudyquismorio@gmail.com>
2023-05-19 21:14:37 +0800

    filter

--- src/routes/blog/[blogId]/+page.ts
+++ src/routes/blog/[blogId]/+page.ts
@@ -5,7 +5,7 @@ import { posts } from '$lib/utility/posts';
 export const load = (async ({ params }) => {
     // load the markdown file based on slug
     const component = await import(`../../../lib/blogs/${params.blogId}.md`);
-    const post = posts.find((post) => post.slug === params.blogId);
+    const post = posts.filter((post) => post.slug === params.blogId)[0];
 
     return {
         blog: params.blogId,
